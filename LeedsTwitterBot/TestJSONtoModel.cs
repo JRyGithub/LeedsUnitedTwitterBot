ef1f9ae01d06261ab328739369a3c55467332c54
Joshua <jryl559@aucklanduni.ac.nz>
2020-09-24 17:56:32 +1200

    dealing with large JSON Locally

--- LeedsTwitterBot/TestJSONtoModel.cs
+++ LeedsTwitterBot/TestJSONtoModel.cs
@@ -1,15 +1,17 @@
 ﻿using LeedsTwitterBot.Models;
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
+using System.Linq;
 using System.Security.Cryptography.X509Certificates;
 
 namespace LeedsTwitterBot
 {
     public  class TestJSONtoModel
     {
-        private readonly string CallerFilePathAttribute = @"C:\Users\Gorilla Rig\Desktop\Projects\LeedsUnitedTwitterBot\LeedsTwitterBot\EvenSmallerJson.Json";
+        private readonly string CallerFilePathAttribute = @"C:\Users\Gorilla Rig\Desktop\Projects\LeedsUnitedTwitterBot\LeedsTwitterBot\MiniJson.Json";
         public async System.Threading.Tasks.Task TestJsonToModelAsync()
         {
             Console.WriteLine("1");
@@ -18,62 +20,39 @@ namespace LeedsTwitterBot
                 string json = r.ReadToEnd();
 
                 var items = JsonConvert.DeserializeObject<GlobalModel>(json);
-                Console.WriteLine("PLAYERS");
+                Teams leeds = null;
+                   
+                foreach(var team in items.Teams)
+                {
+                    if(team.Name == "Leeds")
+                    {
+                        leeds = team;
+                        break;
+                    }                   
+                }
+                List<Player> leedsPlayers = new List<Player>();
 
                 foreach(var player in items.Players)
                 {
-                    Console.WriteLine("Name: "+ player.FirstName + player.SecondName);
-                    Console.WriteLine("Position: " + player.Position);
-                }
-
-                Console.WriteLine("GAME SETTINGS");
-                Console.WriteLine("LeagueJoinPublicMax " + items.GameSettings.LeagueJoinPublicMax);
-                Console.WriteLine("TieBreak " + items.GameSettings.LeagueH2hTiebreakStats[0]);
-
-                Console.WriteLine("STATS");
-                foreach (var stat in items.StatsOptions)
-                {
-                    Console.WriteLine("Name of Stat: " + stat.Name);
-                }
-
-                Console.WriteLine("Phases");
-                foreach (var phase in items.Phases)
-                {
-                    Console.WriteLine("Name of Phase: " + phase.Name);
-                }
-
-                Console.WriteLine("TOTAL PLAYERS: " + items.TotalPlayers);
-
-                Console.WriteLine("ELEMENTTYPE");
-                foreach (var element in items.PlayerTypes)
-                {
-                    Console.WriteLine("Name: " + element.SingularName);
-                    Console.WriteLine("SubPosLocked" + element.SubPositionsLocked[0]);
-                }
-
-                Console.WriteLine("TEAMS");
-                foreach (var team in items.Teams)
-                {
-                    Console.WriteLine("Name: " + team.Name);
-                }
-
-                Console.WriteLine("EVENTS");
-                foreach (var event1 in items.Events)
-                {
-                    Console.WriteLine("Name: " + event1.Name);
-                    if (event1.TopElementInfo != null)
+                    if (player.TeamCode == 2)
                     {
-                        Console.WriteLine("TopElementInfo:" + event1.TopElementInfo.Id);
+                        leedsPlayers.Add(player);
                     }
-                        foreach (var chip in event1.ChipPlays)
-                    {
-                        Console.WriteLine("ChipName: " + chip.ChipName);
-                    }
-                }             
-
-
+                }
+                Console.WriteLine("Leeds Players:");
+                foreach (var player in leedsPlayers)
+                {
+                    Console.WriteLine(player.FirstName + " "+ player.SecondName);
+                }
             }
+
         }
-        
+        public static string GetAllProperties(object obj)
+        {
+            return string.Join(" ", obj.GetType()
+                                        .GetProperties()
+                                        .Select(prop => prop.GetValue(obj)));
+        }
+
     }
 }
